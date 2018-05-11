67d99f75dfa121c4a998a98f50d603156aba7e21
FluorineDog <fluorine@gmail.com>
2018-05-12 02:44:48 +0800

    init

---
+++ spider/analyse.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+from bs4 import BeautifulSoup
+
+def walker(soup):
+    if soup.name is not None:
+        print(str(soup.name) + ":" + str(type(soup)))
+        for child in soup.children:
+            #process node
+            walker(child)
+string = '''
+<div class="more-items">
+</div>
+'''
+def main():
+  with open("html/top250.0.html") as file:
+    raw_data = file.read()
+    soup = BeautifulSoup(raw_data, "lxml")
+    body = soup.body
+    
+    # print("\n********************************\n"
+      # .join([repr(x) for x in body]))
+    divs = body.find_all("div")
+
+    wrapper = [
+      x for x in divs if 
+      x.name is not None 
+      and x.attrs.get('id') == 'wrapper'
+    ][0]
+    movielist = [x for x in wrapper.div.ol.children if x.name ]
+    for movie in movielist:
+      addr = movie.find("a")
+      print(movie)
+      print("**********")
+
+main()
---
+++ spider/readme.md
@@ -0,0 +1,6 @@
+Crime, Sex and Stupid Spider, Part I: Stupid Spider
+----------------------
+
+1. Visit Douban TOP 250, store it into html folder
+2. analyse 1., got movie address
+3. goto movie address, 
